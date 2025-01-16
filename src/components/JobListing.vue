a249813c9b0429945782d2a3225b10dec0ecd0c8
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-16 13:43:25 +0100

    Ajout de la gestion de la navigation avec vue-router, intégration de PrimeIcons et création des vues pour les emplois et la page d'accueil.

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,9 +1,25 @@
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, ref, computed } from 'vue'
+import { RouterLink } from 'vue-router';
 
-defineProps({
+const props = defineProps({
     job: Object
-})
+});
+
+const showFullDescription = ref(false);
+
+const toggleFullDescription = () => {
+    showFullDescription.value = !showFullDescription.value;
+};
+
+const truncatedDescription = computed(() => {
+    let description = props.job.description;
+    if (!showFullDescription.value) {
+        description = description.substring(0, 90) + '...';
+    }
+
+    return description;
+});
 </script>
 
 <template>
@@ -11,11 +27,16 @@ defineProps({
         <div class="p-4">
         <div class="mb-6">
             <div class="text-gray-600 my-2">{{ job.type }}</div>
-            <h3 class="text-xl font-bold">{{ Job.title }}</h3>
+            <h3 class="text-xl font-bold">{{ job.title }}</h3>
         </div>
 
         <div class="mb-5">
-            {{ job.description }}
+            <div>
+                {{ truncatedDescription }}
+            </div>
+            <button class="text-green-500 hover:text-green-600" @click="toggleFullDescription">
+                {{ showFullDescription ? 'Show Less' : 'Show More' }}
+            </button>
         </div>
 
         <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
@@ -24,15 +45,15 @@ defineProps({
 
         <div class="flex flex-col lg:flex-row justify-between mb-4">
             <div class="text-orange-700 mb-3">
-            <i class="fa-solid fa-location-dot text-lg"></i>
+            <i class="pi pi-map-marker text-orange-700"></i>
                 <span>{{ job.location }}</span>
             </div>
-            <a
-            :href="'/job/' + job.id"
+            <RouterLink
+            :to="'/job/' + job.id"
             class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
             >
             Read More
-            </a>
+            </RouterLink>
         </div>
         </div>
     </div>
