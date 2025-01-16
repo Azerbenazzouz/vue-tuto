94c4d3d00c7ae7afe90eefcd24c2d205b198a69c
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-16 11:44:10 +0100

    Ajout de nouveaux composants Vue pour la gestion des offres d'emploi et refactorisation de la structure de l'application

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import Hero from './components/Hero.vue'
+import HomeCards from './components/HomeCards.vue'
+import JobListings from './components/JobListings.vue'
+</script>
 <template>
-  <h1 class="text-2xl">Vue Jobs</h1>
+  <NavBar />
+  <Hero title="Test Title" subtitle="Test SubTitle"/>
+  <HomeCards />
+  <JobListings />
 </template>
